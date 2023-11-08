236eb4d64811d342f6de30b5d42537cf1d01fcff
prateek271 <prateekpatriotasme@gmail.com>
2023-11-09 01:58:15 +0530

    run file setup

--- WS_flask_app/run.py
+++ WS_flask_app/run.py
@@ -1,26 +1,30 @@
-from flask import Flask
+# run flak server
+
+from app.views import app
 from config import setup,ErrorHndle
 from threading import Thread
 
-app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Welcome to the WeSHARE API!"
+# if __name__ == '__main__':
+#     configure_env=Thread(target=setup)
+#     configure_env.start()
+#     print("\n","Starting Flask Server".center(50,"-"),"\n")
+#     try:
+#         app.run(debug=True)
+#     except Exception as e:
+#         ErrorHndle().handle_and_exit(mesaage="Server Stopped",e=str(e))
+#     finally:
+#         # configure_env.join()
+#         print("\n","Server Stopped".center(50,"-"),"\n")
 
-@app.route('/<string:endpoint>')
-def endpoint(endpoint):
-    return f"No page on \t\' http://localhost/{endpoint} \'"
 
-# run flak server
-if __name__ == '__main__':
-    configure_env=Thread(target=setup)
-    configure_env.start()
-    print("\n","Starting Flask Server".center(50,"-"),"\n")
-    try:
-        app.run(debug=True)
-    except KeyboardInterrupt or Exception as e:
-        ErrorHndle().handle_and_exit(mesaage="Server Stopped",e=e)
-    finally:
-        # configure_env.join()
-        print("\n","Server Stopped".center(50,"-"),"\n")
+configure_env=Thread(target=setup)
+configure_env.start()
+print("\n","Starting Flask Server".center(50,"-"),"\n")
+try:
+    app.run(debug=True)
+except Exception as e:
+    ErrorHndle().handle_and_exit(mesaage="Server Stopped",e=str(e))
+finally:
+    # configure_env.join()
+    print("\n","Server Stopped".center(50,"-"),"\n")
