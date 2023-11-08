cf566e44d8bd891a6bfef845ec9fd95e3f3de007
prateek271 <prateekpatriotasme@gmail.com>
2023-11-08 22:32:49 +0530

    initial phase working

--- WS_flask_app/run.py
+++ WS_flask_app/run.py
@@ -1,13 +1,17 @@
 from flask import Flask
 from config import setup,ErrorHndle
+from threading import Thread
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World!"
+    return "Welcome to the WeSHARE API!"
 
+
+# run flak server
 if __name__ == '__main__':
-    
-    setup()
-    app.run(debug=True)
+    configure_env=Thread(target=setup)
+    configure_env.start()
+    print("\n","Starting Flask Server".center(50,"-"),"\n")
+    app.run()
