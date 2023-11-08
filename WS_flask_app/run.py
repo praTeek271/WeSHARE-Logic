71f8844894ae75e4f5886118589ad557c9e3e47e
prateek271 <prateekpatriotasme@gmail.com>
2023-11-09 00:58:57 +0530

    handling bad endendpoints

--- WS_flask_app/run.py
+++ WS_flask_app/run.py
@@ -8,10 +8,19 @@ app = Flask(__name__)
 def home():
     return "Welcome to the WeSHARE API!"
 
+@app.route('/<string:endpoint>')
+def endpoint(endpoint):
+    return f"No page on \t\' http://localhost/{endpoint} \'"
 
 # run flak server
 if __name__ == '__main__':
     configure_env=Thread(target=setup)
     configure_env.start()
     print("\n","Starting Flask Server".center(50,"-"),"\n")
-    app.run()
+    try:
+        app.run(debug=True)
+    except KeyboardInterrupt or Exception as e:
+        ErrorHndle().handle_and_exit(mesaage="Server Stopped",e=e)
+    finally:
+        # configure_env.join()
+        print("\n","Server Stopped".center(50,"-"),"\n")
